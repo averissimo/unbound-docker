c13bf4d0ad7a5fb632e687a6890b2738becb1098
André Veríssimo <afsverissimo@gmail.com>
2020-04-18 15:15:58 +0200

    unbound-anchor only runs if root.key doesn't exist

--- entry.sh
+++ entry.sh
@@ -376,8 +376,12 @@ mkdir -p /opt/unbound/etc/unbound/dev && \
 cp -a /dev/random /dev/urandom /opt/unbound/etc/unbound/dev/
 
 mkdir -p -m 700 /opt/unbound/etc/unbound/var && \
-chown _unbound:_unbound /opt/unbound/etc/unbound/var && \
-/opt/unbound/sbin/unbound-anchor -a /opt/unbound/etc/unbound/var/root.key
+chown _unbound:_unbound /opt/unbound/etc/unbound/var
+
+# Only build root.key if it doesn't exist already
+if [ ! -f /opt/unbound/etc/unbound/var/root.key ]; then
+	/opt/unbound/sbin/unbound-anchor -a /opt/unbound/etc/unbound/var/root.key
+fi
 
 exec /opt/unbound/sbin/unbound -d -c /opt/unbound/etc/unbound/unbound.conf
 
